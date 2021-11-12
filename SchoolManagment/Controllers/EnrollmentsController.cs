f8a9593e1743a7e6d899ef8a09f94a95adcab21b
Alam Ferrera <alamferrera@gmail.com>
2021-11-12 16:07:17 -0400

    nuevos controllers and view
    nuevos controladores y vistas agregados

---
+++ SchoolManagment/Controllers/EnrollmentsController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SchoolManagment.Models;
+
+namespace SchoolManagment.Controllers
+{
+    public class EnrollmentsController : Controller
+    {
+        private SchoolManagmentEntities db = new SchoolManagmentEntities();
+
+        // GET: Enrollments
+        public async Task<ActionResult> Index()
+        {
+            var enrollments = db.Enrollments.Include(e => e.Course).Include(e => e.Student).Include(e => e.Lecturer);
+            return View(await enrollments.ToListAsync());
+        }
+
+        // GET: Enrollments/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = await db.Enrollments.FindAsync(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Create
+        public ActionResult Create()
+        {
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseId", "Title");
+            ViewBag.StudentID = new SelectList(db.Students, "StudentId", "FirstName");
+            ViewBag.LecturerID = new SelectList(db.Lecturers, "Id", "FirstName");
+            return View();
+        }
+
+        // POST: Enrollments/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "EnrollmentId,Grade,CourseID,StudentID,LecturerID")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Enrollments.Add(enrollment);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseId", "Title", enrollment.CourseID);
+            ViewBag.StudentID = new SelectList(db.Students, "StudentId", "FirstName", enrollment.StudentID);
+            ViewBag.LecturerID = new SelectList(db.Lecturers, "Id", "FirstName", enrollment.LecturerID);
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = await db.Enrollments.FindAsync(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseId", "Title", enrollment.CourseID);
+            ViewBag.StudentID = new SelectList(db.Students, "StudentId", "FirstName", enrollment.StudentID);
+            ViewBag.LecturerID = new SelectList(db.Lecturers, "Id", "FirstName", enrollment.LecturerID);
+            return View(enrollment);
+        }
+
+        // POST: Enrollments/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "EnrollmentId,Grade,CourseID,StudentID,LecturerID")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(enrollment).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseId", "Title", enrollment.CourseID);
+            ViewBag.StudentID = new SelectList(db.Students, "StudentId", "FirstName", enrollment.StudentID);
+            ViewBag.LecturerID = new SelectList(db.Lecturers, "Id", "FirstName", enrollment.LecturerID);
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = await db.Enrollments.FindAsync(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // POST: Enrollments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Enrollment enrollment = await db.Enrollments.FindAsync(id);
+            db.Enrollments.Remove(enrollment);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
