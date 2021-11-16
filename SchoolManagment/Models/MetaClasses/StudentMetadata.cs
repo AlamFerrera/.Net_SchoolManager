459df84e50aabbc185780a357a5ee7459da51ad3
Alam Ferrera <alamferrera@gmail.com>
2021-11-16 10:26:18 -0400

    added metadata files
    metadata

---
+++ SchoolManagment/Models/MetaClasses/StudentMetadata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SchoolManagment.Models
+{
+    public class StudentMetadata
+    {
+        [StringLength(50)]
+        [Display(Name = "Nombres")]
+        public string FirstName { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Apellidos")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Fecha de Inscripción")]
+        public Nullable<System.DateTime> EnrollmentDate { get; set; }
+    }
+
+    [MetadataType(typeof(StudentMetadata))]
+    public partial class Student{}
+}
